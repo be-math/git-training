401400e3dc51be45c3cc470f38d9496e6f67b430
mqy <mquievy@automatic-systems.com>
2021-03-03 22:36:56 +0100

    m10

--- new.sh
+++ new.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 if [[ $1 != "" ]]; then
   if [ "$2" = "main" -o "$2" = "" ]; then
-    echo "new mod and commit $1 on branch master"
-    git co master > /dev/null 2>&1
+    echo "new mod and commit $1 on branch main"
+    git co main > /dev/null 2>&1
     echo "$1" >> main
     
   else
-    echo "new mod and commit $1 on branch test"
-    git co test > /dev/null 2>&1
+    echo "new mod and commit $1 on branch $2"
+    git co $2 > /dev/null 2>&1
     echo "$1" >> fct
   fi
   git add .
